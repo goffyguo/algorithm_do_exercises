a36f12b9c9b39f385bc493bd6a44b1c0a6821122
guofei <guogoffy@outlook.com>
2022-02-07 17:20:49 +0800

    add HJ11 and HJ12

---
+++ src/main/java/nowcoderhuawei/HJ11.java
@@ -0,0 +1,27 @@
+package nowcoderhuawei;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuoFei
+ * @Date: 2022/02/07/14:54
+ * @Description: 数字颠倒
+ *
+ */
+public class HJ11 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        char[] chars = s.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        for (int i = chars.length-1; i >=0 ; i--) {
+            builder.append(chars[i]);
+        }
+        System.out.println(builder);
+    }
+}
---
+++ src/main/java/nowcoderhuawei/HJ12.java
@@ -0,0 +1,25 @@
+package nowcoderhuawei;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuoFei
+ * @Date: 2022/02/07/15:13
+ * @Description: 字符串反转
+ */
+public class HJ12 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            String s = scanner.nextLine();
+            char[] chars = s.toCharArray();
+            for (int i = chars.length-1; i >=0 ; i--) {
+                System.out.print(chars[i]);
+            }
+            System.out.println();
+        }
+    }
+}
